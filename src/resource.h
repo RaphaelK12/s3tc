2fc5a8d2613da6e8f9ebe2dcf56a4a9aa135acdb
Maksym Veremeyenko <verem@m1.tv>
2011-12-20 14:51:50 +0200

    Initial load

---
+++ src/resource.h
@@ -0,0 +1,25 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by ui.rc
+//
+#define IDD_ABOUT                       101
+#define IDD_CONFIGURE                   102
+#define IDC_HOMEPAGE                    1000
+#define IDC_EMAIL                       1001
+#define IDC_COMPRESSASYUY2              1002
+#define IDC_RGBOUTPUT                   1003
+#define IDC_LOG                         1004
+#define IDC_STATIC_VER1                 1006
+#define IDC_STATIC_VER2                 1007
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
